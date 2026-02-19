6f65864e8c4a226957a6b855ddcf26a16833a222
YosrJemal <yosr.jemal@tbs.u-tunis.tn>
2026-02-19 12:17:32 +0100

    Week 2: First React component with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,32 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const courseTitle = "Advanced Web Development"
 
+function App (){
+  const studentName ='Jmal Yosr'
+  const student = {
+    name: 'Jmal',
+    age: 20,
+    track: 'Business',
+  }
+  function sayHello(){
+    return 'Hello ${studentName}'
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1> Hello everyone</h1>
+      <p> my name is {studentName}</p>
+      <p> Course: {courseTitle}</p>
+      <p> Age: {student.age}</p>
+      <p> Track: {student.track}</p>
+    </div>
   )
 }
 
-export default App
+
+export default App;
+
+  
+
+  
